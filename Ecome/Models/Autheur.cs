86806d573f83360b6684707f8d65e9693f2aec98
malun <malun@MALUNZI>
2022-12-05 23:19:03 +0100

    Ajoutez des fichiers projet.

---
+++ Ecome/Models/Autheur.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecome.Models
+{
+    public class Autheur
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? PhotoProfile { get; set; }
+
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string BiblioGraphie { get; set; }
+
+
+    }
+}
